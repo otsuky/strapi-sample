778a874bddc60220ac0c4a32a19eccd5de3075ad
Y.Otsuki <vloxolv.otky.vlowolv@gmail.com>
2021-09-18 19:22:43 +0900

    backend を docker に置き換え

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ffc3773937cefe1a3d6c9c99d80c4466'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '42242aea-a6a4-4d48-ac82-6d20fa71b5db'
-};
